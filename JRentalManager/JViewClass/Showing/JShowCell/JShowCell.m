45e12ae422ec4b7a2ee402095ae0b704301bfd84
Jone <jone.ji@generalbiologic.com>
2015-05-17 22:44:24 +0800

    add show cell

---
+++ JRentalManager/JViewClass/Showing/JShowCell/JShowCell.m
@@ -0,0 +1,23 @@
+//
+//  JShowCell.m
+//  JRentalManager
+//
+//  Created by Jone on 15/5/17.
+//  Copyright (c) 2015年 Jone. All rights reserved.
+//
+
+#import "JShowCell.h"
+
+@implementation JShowCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
